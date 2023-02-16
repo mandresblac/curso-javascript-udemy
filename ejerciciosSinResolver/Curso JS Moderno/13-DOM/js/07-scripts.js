9755c76891ebec8615776aafe431fca0708a47f3
manuel blanco <mandresblac@gmail.com>
2023-02-15 20:11:53 -0500

    commit 8

--- ejerciciosSinResolver/Curso JS Moderno/13-DOM/js/07-scripts.js
+++ ejerciciosSinResolver/Curso JS Moderno/13-DOM/js/07-scripts.js
@@ -7,17 +7,18 @@ const heading = document.querySelector("h1");
 // y hay todo un objeto con propiedades CSS que puedes utilizar, si quieres conocerlas todas coloca...
 console.log(heading.style);
 
-// heading.style.backgroundColor = 'red'; //  Nota como las propiedades que le puedes pasar, son similares a las de CSS, con la diferencia de que el guion se elimina y la segunda palabra su primer letra es mayuscula.
+heading.style.backgroundColor = "red"; //  Nota como las propiedades que le puedes pasar, son similares a las de CSS, con la diferencia de que el guion se elimina y la segunda palabra su primer letra es mayúscula.
 
-// heading.style.textTransform = 'uppercase';
-// heading.style.fontFamily = 'Arial';
+//heading.style.textTransform = "uppercase";
+//heading.style.fontFamily = "Arial";
+//heading.style.textAlign = "center";
 
 // Ahora yo no recomiendo que pongas style, ya que tu archivo JS será muy grande, otra desventaja es que la apariencia debe ser algo que sea responsabilidad del CSS, pero lo que si puedes hacer es agregar o quitar classes..
 
-// Vamos a seleccionar el primer card, puedes ver que tiene un parrafo con una categoria llamada concierto, eso le cambia el color:
+// Vamos a seleccionar el primer card, puedes ver que tiene un párrafo con una categoría llamada concierto, eso le cambia el color:
 const card = document.querySelector(".card");
 console.log(card);
-console.log(card.classList); // Classlist nos permitirá listar las classes
+console.log(card.classList); // Classlist nos permitirá listar las classes en un array
 card.classList.add("nueva-clase"); // te permitirá añadir una clase.. // si deseas añadir múltiples classes debes agregar una coma en cada una
 
 // ahora si deseas eliminar una clase utilizas.remove
