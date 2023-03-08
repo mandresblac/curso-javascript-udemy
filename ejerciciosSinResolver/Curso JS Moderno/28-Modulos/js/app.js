7174dc7de32b5debdcb396e8ae2f5bba8ce327b7
manuel blanco <mandresblac@gmail.com>
2023-03-08 11:56:10 -0500

    commit 36

---
+++ ejerciciosSinResolver/Curso JS Moderno/28-Modulos/js/app.js
@@ -0,0 +1,27 @@
+// 4
+import nuevaFuncion, {
+  nombreCliente,
+  ahorro,
+  mostrarInformacion,
+  Cliente,
+} from "./cliente.js";
+import { Empresa } from "./empresa.js";
+//import { Cliente } from "./cliente.js";
+
+let cliente = new Cliente(nombreCliente, ahorro);
+console.log(cliente.mostrarInformacion());
+
+//const nombreCliente = "Juan",
+//ahorroCliente = 400;
+
+//let cliente = new Cliente(nombreCliente, ahorroCliente);
+console.log(cliente.mostrarInformacion());
+
+const nombreEmpresa = "Udemy",
+  ahorroEmpresa = 1000000000,
+  categoriaEmpresa = "aprendizaje";
+
+let empresa = new Empresa(nombreEmpresa, ahorroEmpresa, categoriaEmpresa);
+console.log(empresa.mostrarInformacion());
+
+nuevaFuncion();
