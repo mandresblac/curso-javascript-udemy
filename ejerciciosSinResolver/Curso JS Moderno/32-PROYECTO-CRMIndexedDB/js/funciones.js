f0a5a671f1603d04dfe762e17953f5145efd950d
manuel blanco <mandresblac@gmail.com>
2023-03-15 17:36:40 -0500

    commit 41

---
+++ ejerciciosSinResolver/Curso JS Moderno/32-PROYECTO-CRMIndexedDB/js/funciones.js
@@ -0,0 +1,57 @@
+//let DB;
+//const formulario = document.querySelector("#formulario");
+
+function conectarDB() {
+  const abrirConexion = window.indexedDB.open("crm", 1);
+
+  abrirConexion.onerror = function () {
+    console.log("Hubo un error");
+  };
+
+  abrirConexion.onsuccess = function () {
+    DB = abrirConexion.result;
+  };
+}
+
+function imprimirAlerta(mensaje, tipo) {
+  const alerta = document.querySelector(".alerta");
+
+  //Para que la alerta no aparezca multiples veces utilizamos un condicional if
+  //Si no existe previamente una alerta (!alerta) entonces muestra la alerta
+  if (!alerta) {
+    //Crear la alerta
+    const divMensaje = document.createElement("div");
+    divMensaje.classList.add(
+      "px-4",
+      "py-3",
+      "rounded",
+      "max-w-lg",
+      "mx-auto",
+      "mt-6",
+      "text-center",
+      "border",
+      "alerta"
+    );
+
+    if (tipo === "error") {
+      divMensaje.classList.add("bg-red-100", "border-red-400", "text-red-700");
+    } else {
+      divMensaje.classList.add(
+        "bg-green-100",
+        "border-green-400",
+        "text-green-700"
+      );
+    }
+
+    // Mensaje de error
+    divMensaje.textContent = mensaje;
+
+    // Insertar en el DOM
+    formulario.appendChild(divMensaje);
+
+    // Quitar el alert despues de 3 segundos
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
+}
--- ejerciciosSinResolver/Curso JS Moderno/32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
+++ ejerciciosSinResolver/Curso JS Moderno/32-PROYECTO-CRMIndexedDB/js/nuevocliente.js
@@ -3,22 +3,10 @@
   const formulario = document.querySelector("#formulario");
 
   document.addEventListener("DOMContentLoaded", () => {
-    conectarDB();
-
     formulario.addEventListener("submit", validarCliente);
+
+    conectarDB();
   });
-
-  function conectarDB() {
-    const abrirConexion = window.indexedDB.open("crm", 1);
-
-    abrirConexion.onerror = function () {
-      console.log("Hubo un error");
-    };
-
-    abrirConexion.onsuccess = function () {
-      DB = abrirConexion.result;
-    };
-  }
 
   function validarCliente(e) {
     e.preventDefault();
@@ -51,10 +39,6 @@
     const objectStore = transaction.objectStore("crm");
     objectStore.add(cliente);
 
-    transaction.onerror = function () {
-      imprimirAlerta("Hubo un error", "error");
-    };
-
     transaction.oncomplete = function () {
       //console.log("Cliente agregado");
       imprimirAlerta("El cliente se agrego correctamente");
@@ -64,49 +48,9 @@
         window.location.href = "index.html";
       }, 3000);
     };
-  }
 
-  function imprimirAlerta(mensaje, tipo) {
-    const alerta = document.querySelector(".alerta");
-
-    //Para que la alerta no aparezca multiples veces utilizamos un condicional if
-    //Si no existe previamente una alerta (!alerta) entonces muestra la alerta
-    if (!alerta) {
-      //Crear la alerta
-      const divMensaje = document.createElement("div");
-      divMensaje.classList.add(
-        "px-4",
-        "py-3",
-        "rounded",
-        "max-w-lg",
-        "mx-auto",
-        "mt-6",
-        "text-center",
-        "border",
-        "alerta"
-      );
-
-      if (tipo === "error") {
-        divMensaje.classList.add(
-          "bg-red-100",
-          "border-red-400",
-          "text-red-700"
-        );
-      } else {
-        divMensaje.classList.add(
-          "bg-green-100",
-          "border-green-400",
-          "text-green-700"
-        );
-      }
-
-      divMensaje.textContent = mensaje;
-
-      formulario.appendChild(divMensaje);
-
-      setTimeout(() => {
-        divMensaje.remove();
-      }, 3000);
-    }
+    transaction.onerror = function () {
+      imprimirAlerta("Hubo un error", "error");
+    };
   }
 })();
