8c786b4dcb522506ece491cbde4e15a5eaa92ad2
manuel blanco <mandresblac@gmail.com>
2023-03-30 12:07:43 -0500

    commit 59

--- ejerciciosSinResolver/Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
+++ ejerciciosSinResolver/Curso JS Moderno/40-PROYECTO-Calculadora de Propinas con JSON-Server/js/app.js
@@ -1,121 +1,128 @@
-/* Comando para ejecutar desde la consola el archivo API db.json:
+/* Para este proyecto se necesita instalar el paquete json-server, para eso en la terminal escribimos el siguiente comando:   npm install -g json-server */
+/* Luego para ejecutar desde la consola el archivo API db.json con JSON-Server vamos a la carpeta del proyecto que es "40-PROYECTO-Calculadora de Propinas" y utilizamos el siguiente comando:
  json-server --watch db.json --port 4000 */
 
 let cliente = {
-    mesa: "",
-    hora: "",
-    pedido: []
-}
+  mesa: "",
+  hora: "",
+  pedido: [],
+};
 
 const categorias = {
-    1: "Comida",
-    2: "Bebidas",
-    3: "Postres"
+  1: "Comida",
+  2: "Bebidas",
+  3: "Postres",
 };
 
 const btnGuardarCliente = document.querySelector("#guardar-cliente");
 btnGuardarCliente.addEventListener("click", guardarCliente);
 
 function guardarCliente() {
-    const mesa = document.querySelector("#mesa").value;
-    const hora = document.querySelector("#hora").value;
+  const mesa = document.querySelector("#mesa").value;
+  const hora = document.querySelector("#hora").value;
 
-    // Validamos si hay campos vacios
-    const camposVacios = [ mesa, hora ].some( campo => campo === "");
+  // Validamos si hay campos vacios
+  const camposVacios = [mesa, hora].some((campo) => campo === "");
 
-    if(camposVacios) {
-        //Verificar si ya existe una alerta
-        const existeAlerta = document.querySelector(".invalid-feedback")
+  if (camposVacios) {
+    //Verificar si ya existe una alerta
+    const existeAlerta = document.querySelector(".invalid-feedback");
 
-        if(!existeAlerta) {
-            //Generamos alerta
-            const alerta = document.createElement("div") ;
-            alerta.classList.add("invalid-feedback", "d-block", "text-center");
-            alerta.textContent = "Todos los campos son obligatorios";
+    if (!existeAlerta) {
+      //Generamos alerta
+      const alerta = document.createElement("div");
+      alerta.classList.add("invalid-feedback", "d-block", "text-center");
+      alerta.textContent = "Todos los campos son obligatorios";
 
-            //Insertamos la alerta en el documento HTML
-            document.querySelector(".modal-body form").appendChild(alerta);
+      //Insertamos la alerta en el documento HTML
+      document.querySelector(".modal-body form").appendChild(alerta);
 
-            //Eliminamos la alerta despues de 3 segundos
-            setTimeout(() => {
-                alerta.remove();
-            }, 3000);
-        }
-
-        return;
+      //Eliminamos la alerta despues de 3 segundos
+      setTimeout(() => {
+        alerta.remove();
+      }, 3000);
     }
 
-    // Asignar datos del formulario a cliente
-    cliente = { ...cliente, mesa , hora };
+    return;
+  }
 
-    //console.log(cliente);
+  // Asignar datos del formulario a cliente
+  cliente = { ...cliente, mesa, hora };
 
-    // Ocultar ventana modal de bootstrap
-    const modalFormulario = document.querySelector("#formulario");
-    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
-    modalBootstrap.hide();
+  //console.log(cliente);
 
-    // Mostrar las secciones
-    mostrarSecciones();
+  // Ocultar ventana modal de bootstrap
+  const modalFormulario = document.querySelector("#formulario");
+  const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario);
+  modalBootstrap.hide();
 
-    // Obtener platillos de la API de JSON-server
-    obtenerPlatillos();
+  // Mostrar las secciones
+  mostrarSecciones();
+
+  // Obtener platillos de la API de JSON-server
+  obtenerPlatillos();
 }
 
 function mostrarSecciones() {
-    const seccionesOcultas = document.querySelectorAll(".d-none");
-    seccionesOcultas.forEach(seccion => seccion.classList.remove("d-none"));
+  const seccionesOcultas = document.querySelectorAll(".d-none");
+  seccionesOcultas.forEach((seccion) => seccion.classList.remove("d-none"));
 }
 
 function obtenerPlatillos() {
-    const url = "http://localhost:4000/platillos";
+  const url = "http://localhost:4000/platillos";
 
-    fetch(url)
-        .then( respuesta => respuesta.json())
-        .then( resultado => mostrarPlatillos(resultado))
-        .catch( error => console.error(error))
+  fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then((resultado) => mostrarPlatillos(resultado))
+    .catch((error) => console.error(error));
 }
 
 function mostrarPlatillos(platillos) {
-    const contenido = document.querySelector("#platillos .contenido");
+  const contenido = document.querySelector("#platillos .contenido");
 
-    platillos.forEach(platillo => {
-        const row = document.createElement("div");
-        row.classList.add("row", "py-3", "border-top");
+  platillos.forEach((platillo) => {
+    const row = document.createElement("div");
+    row.classList.add("row", "py-3", "border-top");
 
-        // Nombre
-        const nombre = document.createElement("div");
-        nombre.classList.add("col-md-4");
-        nombre.textContent = platillo.nombre;
+    // Nombre
+    const nombre = document.createElement("div");
+    nombre.classList.add("col-md-4");
+    nombre.textContent = platillo.nombre;
 
-        // Precio
-        const precio = document.createElement("div");
-        precio.classList.add("col-md-3", "fw-bold");
-        precio.textContent = `$ ${platillo.precio}`;
+    // Precio
+    const precio = document.createElement("div");
+    precio.classList.add("col-md-3", "fw-bold");
+    precio.textContent = `$ ${platillo.precio}`;
 
-        // Categoria
-        const categoria = document.createElement("div");
-        categoria.classList.add("col-md-3");
-        categoria.textContent = categorias[ platillo.categoria ];
+    // Categoria
+    const categoria = document.createElement("div");
+    categoria.classList.add("col-md-3");
+    categoria.textContent = categorias[platillo.categoria];
 
-        // Input de cantidad
-        const inputCantidad = document.createElement("input");
-        inputCantidad.type = "number";
-        inputCantidad.min = 0;
-        inputCantidad.value = 0;
-        inputCantidad.id = `producto-${platillo.id}`;
-        inputCantidad.classList.add("form-control");
+    // Input de cantidad
+    const inputCantidad = document.createElement("input");
+    inputCantidad.type = "number";
+    inputCantidad.min = 0;
+    inputCantidad.value = 0;
+    inputCantidad.id = `producto-${platillo.id}`;
+    inputCantidad.classList.add("form-control");
 
-        const agregar = document.createElement("div");
-        agregar.classList.add("col-md-2");
-        agregar.appendChild(inputCantidad);
+    // Función que detecta la cantidad y el platillo que se esta agregando
+    inputCantidad.onchange = agregarPlatillo;
 
-        row.appendChild(nombre);
-        row.appendChild(precio);
-        row.appendChild(categoria);
-        row.appendChild(agregar);
+    const agregar = document.createElement("div");
+    agregar.classList.add("col-md-2");
+    agregar.appendChild(inputCantidad);
 
-        contenido.appendChild(row);
-    });
+    row.appendChild(nombre);
+    row.appendChild(precio);
+    row.appendChild(categoria);
+    row.appendChild(agregar);
+
+    contenido.appendChild(row);
+  });
 }
 
+function agregarPlatillo() {
+  console.log();
+}
