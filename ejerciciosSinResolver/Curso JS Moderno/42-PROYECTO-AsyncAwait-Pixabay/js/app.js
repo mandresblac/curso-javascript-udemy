98034bc30ee5f16ed2b765bd6c0ff1097a064a3e
manuel blanco <mandresblac@gmail.com>
2023-04-03 21:20:13 -0500

    commit 62

--- ejerciciosSinResolver/Curso JS Moderno/42-PROYECTO-AsyncAwait-Pixabay/js/app.js
+++ ejerciciosSinResolver/Curso JS Moderno/42-PROYECTO-AsyncAwait-Pixabay/js/app.js
@@ -14,9 +14,9 @@ window.onload = () => {
 
 function validarFormulario(e) {
     e.preventDefault();
-    
+
     const terminoBusqueda = document.querySelector('#termino').value;
-    
+
     if(terminoBusqueda === '' ) {
         mostrarAlerta('Agrega un término de búsqueda');
         return;
@@ -32,35 +32,44 @@ function mostrarAlerta(mensaje) {
     if(!existeAlerta) {
         const alerta = document.createElement('p');
         alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
-    
+
         alerta.innerHTML = `
             <strong class="font-bold">Error!</strong>
             <span class="block sm:inline">${mensaje}</span>
         `;
-    
+
         formulario.appendChild(alerta);
-    
+
         setTimeout(() => {
             alerta.remove();
         }, 3000);
     }
-    
+
 
 }
 
-function buscarImagenes() {
+async function buscarImagenes() {
 
     const termino = document.querySelector('#termino').value;
 
-    const key = '';
+    const key = '34622719-b085ead1d49a26ab9d1e11cb0';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
 
-    fetch(url)
+    /* fetch(url)
         .then(respuesta => respuesta.json())
         .then(resultado => {
             totalPaginas = calcularPaginas(resultado.totalHits);
             mostrarImagenes(resultado.hits);
-        })
+        }) */
+
+        try {
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+            totalPaginas = calcularPaginas(resultado.totalHits);
+            mostrarImagenes(resultado.hits);
+        } catch (error) {
+            console.log(error);
+        }
 }
 
 // Generador que va a registrar la cantidad de elementos de acuerdo a las paginas
@@ -77,7 +86,7 @@ function calcularPaginas(total) {
 
 function mostrarImagenes(imagenes) {
     // console.log(imagenes);
-    
+
     while(resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
     }
@@ -95,9 +104,9 @@ function mostrarImagenes(imagenes) {
                         <p class="font-bold"> ${likes} <span class="font-light"> Me Gusta </span> </p>
                         <p class="font-bold"> ${views} <span class="font-light"> Veces Vista </span> </p>
 
-                        <a 
+                        <a
                             class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
-                            href="${largeImageURL}" target="_blank" rel="noopener noreferrer" 
+                            href="${largeImageURL}" target="_blank" rel="noopener noreferrer"
                         >
                             Ver Imagen
                         </a>
