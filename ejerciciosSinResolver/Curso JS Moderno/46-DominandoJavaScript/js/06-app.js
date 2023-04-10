c1fe7012657490ce0101217e80cc9c9ca83fe997
manuel blanco <mandresblac@gmail.com>
2023-04-10 10:50:01 -0500

    commit 67

---
+++ ejerciciosSinResolver/Curso JS Moderno/46-DominandoJavaScript/js/06-app.js
@@ -0,0 +1,19 @@
+// new binding
+
+/** También cuando creas un nuevo objeto con el object constructor tienes acceso a la palabra this via un nuevo objeto con new */
+
+function Auto(modelo, color) {
+    this.modelo = modelo;
+    this.color = color;
+}
+const auto = new Auto('Camaro', 'Negro');
+console.log(auto);
+
+// Window binding
+
+window.color = 'negro';
+function hola() {
+    console.log(color);
+}
+
+hola();
