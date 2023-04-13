b3c8a75040e242367de4f4dde581756be360102f
manuel blanco <mandresblac@gmail.com>
2023-04-13 14:40:50 -0500

    commit 84

---
+++ ejerciciosSinResolver/Curso JS Moderno/52-Testing-Cypress/cypress/e2e/primer_test.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+describe("Carga la pagina principal", () => {
+    it("Carga lapagina principal", () => {
+
+        cy.visit("http://127.0.0.1:5500/52-Testing-Cypress/index.html");
+
+        // Verificar el elemento y su texto
+        cy.contains("[data-cy='titulo-proyecto']", "Administrador de Pacientes de Veterinaria");
+
+        // Verificar que exista
+        cy.get("[data-cy='titulo-proyecto']").should("exist");
+
+        // Verificar que exista el elemento y contenga un texto en especifico
+        cy.get("[data-cy='titulo-proyecto']")
+            .invoke("text")
+            .should("equal", "Administrador de Pacientes de Veterinaria");
+
+        // Verificar el texto de las citas
+        cy.get("[data-cy=citas-heading]")
+            .invoke("text")
+            .should("equal", "No hay Citas, comienza creando una");
+    });
+})
