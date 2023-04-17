31a2deac287e0f3497888ce051043c440aa554d9
manuel blanco <mandresblac@gmail.com>
2023-04-16 21:33:02 -0500

    commit 96

--- ejerciciosSinResolver/Curso JS Moderno/agenciaViajesNode/index.js
+++ ejerciciosSinResolver/Curso JS Moderno/agenciaViajesNode/index.js
@@ -1,41 +1,27 @@
+/* Comando para correr la aplicacion en la conosola:  npm run dev */
+
+//const express = require('express');
 import express from 'express';
-import router from './routes/index.js';
-import db from './config/db.js';
 
 const app = express();
 
-// Conectar la base de datos
-db.authenticate()
-    .then( () => console.log('Base de datos conectada') )
-    .catch( error => console.log(error));
+//Definimos puerto
+const port = process.env.PORT || 4000;
 
-
-// Definir puerto
-const port = process.env.PORT || 3000;
-
-// Habilitar PUG
-app.set('view engine', 'pug');
-
-// Obtener el año actual
-app.use( (req, res, next) => {
-    const year = new Date();
-    res.locals.actualYear = year.getFullYear();
-    res.locals.nombresitio = "Agencia de Viajes";
-    next();
+//Metodo get de HTTP
+// req = request o "peticion, solicitud", res = response o "respuesta"
+app.get("/", (req, res) => {
+    res.send("Inicio");
 });
 
+app.get("/nosotros", (req, res) => {
+    res.send("Nosotros");
+});
 
-// Habilitar express.json
-app.use(express.urlencoded({ extended: false }));
-
-
-// Definir la carpeta publica
-app.use(express.static('public'));
-
-// Agregar Router
-app.use('/', router);
-
+app.get("/contacto", (req, res) => {
+    res.send("Contacto");
+});
 
 app.listen(port, () => {
-    console.log(`El Servidor esta funcionando en el puerto ${port}`)
-})
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
+});
