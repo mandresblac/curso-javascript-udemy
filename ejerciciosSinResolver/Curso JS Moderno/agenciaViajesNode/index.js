8a8eaf650fd072fa7368c3a26b0fead1ab6bc87c
manuel blanco <mandresblac@gmail.com>
2023-04-16 21:35:52 -0500

    commit 97

--- ejerciciosSinResolver/Curso JS Moderno/agenciaViajesNode/index.js
+++ ejerciciosSinResolver/Curso JS Moderno/agenciaViajesNode/index.js
@@ -8,19 +8,6 @@ const app = express();
 //Definimos puerto
 const port = process.env.PORT || 4000;
 
-//Metodo get de HTTP
-// req = request o "peticion, solicitud", res = response o "respuesta"
-app.get("/", (req, res) => {
-    res.send("Inicio");
-});
-
-app.get("/nosotros", (req, res) => {
-    res.send("Nosotros");
-});
-
-app.get("/contacto", (req, res) => {
-    res.send("Contacto");
-});
 
 app.listen(port, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}`);
--- ejerciciosSinResolver/Curso JS Moderno/agenciaViajesNode/routes/index.js
+++ ejerciciosSinResolver/Curso JS Moderno/agenciaViajesNode/routes/index.js
@@ -2,3 +2,17 @@ import express from 'express';
 
 const router = express.Router();
 
+//Metodo get de HTTP
+// req = request o "peticion, solicitud", res = response o "respuesta"
+app.get("/", (req, res) => {
+    res.send("Inicio");
+});
+
+app.get("/nosotros", (req, res) => {
+    res.send("Nosotros");
+});
+
+app.get("/contacto", (req, res) => {
+    res.send("Contacto");
+});
+
