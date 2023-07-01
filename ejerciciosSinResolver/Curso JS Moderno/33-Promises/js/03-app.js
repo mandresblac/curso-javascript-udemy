41f31b8825ef97940070604ec5c9ab7cb6db852d
manuel blanco <mandresblac@gmail.com>
2023-06-30 20:27:03 -0500

    commit 113

--- ejerciciosSinResolver/Curso JS Moderno/33-Promises/js/03-app.js
+++ ejerciciosSinResolver/Curso JS Moderno/33-Promises/js/03-app.js
@@ -1,22 +1,22 @@
-
 // 1- Definimos una promise o promesa
-const aplicarDescuento = new Promise( (resolve, reject) => { // Puede ser arrow function...
-    const descuento = true;
+const aplicarDescuento = new Promise((resolve, reject) => {
+  // Puede ser arrow function...
+  const descuento = true;
 
-    // Comentar estas siguientes lineas para ver el Resolve...
-    if(descuento) {
-        resolve('Descuento Aplicado');
-    } else {
-        reject('No se pudo aplicar el descuento');
-    }
+  // Comentar estas siguientes lineas para ver el Resolve...
+  if (descuento) {
+    resolve("Descuento Aplicado");
+  } else {
+    reject("No se pudo aplicar el descuento");
+  }
 });
 
 // 2- Utilizamos el Promise
 aplicarDescuento
-    .then( resultado => console.log(resultado))
-    .catch( error => console.log(error));
+  .then((resultado) => console.log(resultado))
+  .catch((error) => console.log(error));
 
-    console.log(aplicarDescuento);
+console.log(aplicarDescuento);
 
 /*
 En los Promises hay 3 valores posibles:
@@ -25,11 +25,10 @@ En los Promises hay 3 valores posibles:
 - pending - La promesa (o Promise) está pendiente, es decir, no se ha cumplido ni tampoco ha sido rechazada
 */
 
-
 // recuerda que gracias a las ventajas de los  arrow functions puedes colocar todo en una sola linea...
 
 // Tal vez tengas la duda de si puedes ejecutar funciones para no tener mucho código, la respuesta es si...
 
 function descuento() {
-    console.log('Aplicando el Descuento...');
+  console.log("Aplicando el Descuento...");
 }
