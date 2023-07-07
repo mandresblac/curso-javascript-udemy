33b3959d8fb2627311a2f272951349777a42e8cc
manuel blanco <mandresblac@gmail.com>
2023-07-07 17:11:48 -0500

    commit 125

--- ejerciciosSinResolver/Curso JS Moderno/47-ServiceWorkers-PWA/sw.js
+++ ejerciciosSinResolver/Curso JS Moderno/47-ServiceWorkers-PWA/sw.js
@@ -1,54 +1,52 @@
 const nombreCache = "apv-v3";
 const archivos = [
-    "/",
-    "/index.html",
-    "/error.html",
-    "/css/bootstrap.css",
-    "/css/styles.css",
-    "/js/app.js",
-    "/js/apv.js"
+  "/",
+  "/index.html",
+  "/error.html",
+  "/css/bootstrap.css",
+  "/css/styles.css",
+  "/js/app.js",
+  "/js/apv.js",
 ];
 
 // Cuando se instala el service worker
-self.addEventListener("install", e => {
-    console.log("Instalado el Service Worker...");
+self.addEventListener("install", (e) => {
+  console.log("Instalado el Service Worker...");
 
-    e.waitUntil(
-        caches.open(nombreCache)
-            .then( cache => {
-                console.log("Cacheando");
-                cache.addAll(archivos);
-            })
-    )
+  e.waitUntil(
+    caches.open(nombreCache).then((cache) => {
+      console.log("Cacheando");
+      cache.addAll(archivos);
+    })
+  );
 });
 
 // Activar el Service Worker
-self.addEventListener("activate", e => {
-    console.log("Service Worker activado...");
+self.addEventListener("activate", (e) => {
+  console.log("Service Worker activado...");
 
-    e.waitUntil(
-        caches.keys()
-            .then( keys => {
-                /* console.log(keys); */
-                return Promise.all(
-                    keys.filter( key => key !== nombreCache )
-                        .map( key => caches.delete(key)) // Borra las versiones anteriores
-                )
-            })
-    )
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      /* console.log(keys); */
+      return Promise.all(
+        keys
+          .filter((key) => key !== nombreCache)
+          .map((key) => caches.delete(key)) // Borra las versiones anteriores
+      );
+    })
+  );
 });
 
 // Evento fetch para descargar archivos estaticos
-self.addEventListener("fetch", e => {
-    console.log("Fetch...", e);
+self.addEventListener("fetch", (e) => {
+  console.log("Fetch...", e);
 
-    e.respondWith(
-        caches.match(e.request)
-            .then( respuestaCache => {
-                return respuestaCache;
-            })
-            .catch( () => caches.match("/error.html") )
-    )
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then((respuestaCache) => {
+        return respuestaCache;
+      })
+      .catch(() => caches.match("/error.html"))
+  );
 });
-
-
