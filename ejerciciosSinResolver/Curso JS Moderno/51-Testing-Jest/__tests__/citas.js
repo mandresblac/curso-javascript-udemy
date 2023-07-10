a81a18379d09625dbff5626ab7f43394a6b37790
manuel blanco <mandresblac@gmail.com>
2023-07-09 22:42:21 -0500

    commit 130

--- ejerciciosSinResolver/Curso JS Moderno/51-Testing-Jest/__tests__/citas.js
+++ ejerciciosSinResolver/Curso JS Moderno/51-Testing-Jest/__tests__/citas.js
@@ -1,48 +1,47 @@
-import Citas from '../js/classes/Citas'
+// Utiliza el siguiente comando para correr este archivo: npm t -- -u
+import Citas from "../js/classes/Citas";
 
-describe('Probar la clase de citas', () => {
+describe("Probar la clase de citas", () => {
+  const citas = new Citas();
+  const id = Date.now();
 
-    const citas = new Citas();
-    const id = Date.now();
+  // Prueba 1
+  test("Agregar una nueva cita", () => {
+    const citaObj = {
+      id,
+      mascota: "Hook",
+      propietario: "Manuel",
+      telefono: "19082737636",
+      fecha: "10-12-202",
+      hora: "10:30",
+      sintomas: "Solo duerme",
+    };
 
-    // Prueba 1
-    test('Agregar una nueva cita', () => {
-        const citaObj = {
-            id,
-            mascota: 'Hook',
-            propietario: 'Manuel',
-            telefono: '19082737636',
-            fecha: '10-12-202',
-            hora:'10:30',
-            sintomas: 'Solo duerme'
-        }
+    citas.agregarCita(citaObj);
 
-        citas.agregarCita(citaObj);
+    // Prueba
+    expect(citas).toMatchSnapshot();
+  });
 
-        // Prueba
-        expect(citas).toMatchSnapshot();
-    });
+  test("Actualizar cita", () => {
+    const citaActualizada = {
+      id,
+      mascota: "Nuevo nombre",
+      propietario: "Roberto",
+      telefono: "19082737636",
+      fecha: "10-12-202",
+      hora: "10:30",
+      sintomas: "Solo duerme",
+    };
 
-    test('Actualizar cita', () => {
-        const citaActualizada = {
-            id,
-            mascota: 'Nuevo nombre',
-            propietario: 'Roberto',
-            telefono: '19082737636',
-            fecha: '10-12-202',
-            hora:'10:30',
-            sintomas: 'Solo duerme'
-        }
+    citas.editarCita(citaActualizada);
 
-        citas.editarCita(citaActualizada);
+    expect(citas).toMatchSnapshot();
+  });
 
-        expect(citas).toMatchSnapshot();
-    })
+  test("Eliminar cita", () => {
+    citas.eliminarCita(id);
 
-    test('Eliminar cita', () => {
-        citas.eliminarCita(id);
-
-        expect(citas).toMatchSnapshot();
-    })
-
+    expect(citas).toMatchSnapshot();
+  });
 });
