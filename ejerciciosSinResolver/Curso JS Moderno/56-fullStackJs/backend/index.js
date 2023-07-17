41e50cb777a92fccf892d1f39c157c475d72f471
manuel blanco <mandresblac@gmail.com>
2023-07-17 16:42:05 -0500

    commit 143

--- ejerciciosSinResolver/Curso JS Moderno/56-fullStackJs/backend/index.js
+++ ejerciciosSinResolver/Curso JS Moderno/56-fullStackJs/backend/index.js
@@ -1,2 +1,18 @@
-// Comando para correr aplicación desde la terminal: npm run start
+// Comando para correr aplicación desde la terminal: npm run dev
 import express from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
+import veterinarioRoutes from "./routes/veterinarioRoutes.js";
+
+const app = express();
+dotenv.config();
+
+conectarDB();
+
+app.use("/api/veterinarios", veterinarioRoutes);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en el puerto ${PORT}`);
+});
